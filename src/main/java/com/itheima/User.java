ca7f1d1c2b589cd0ee348caf891d48b09432a0e8
qjf <617388912@qq.com>
2019-08-10 22:01:55 +0800

    添加到本地仓库

---
+++ src/main/java/com/itheima/User.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+/**
+ * @ Author     ：qjf
+ * @ Date       ：Created in 21:58 2019/8/10
+ * @ Description：
+ * @ Modified By：
+ */
+public class User {
+    private String name;
+}
